be729ecbaa305062ab99b1e41c5b1c3f4bc5e3b5
max-im <pogidaevmo@gmail.com>
2024-05-19 14:24:36 +0300

    add header styling

--- client/src/Header/Header.tsx
+++ client/src/Header/Header.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Box, Stack, Typography } from '@mui/material';
 
 export default function Header() {
   return (
-    <div>Header
-      <Link to="/login">Login</Link>
-      <Link to="/users">Users</Link>
-      <Link to="/logout">Logout</Link>
-      <Link to="/users">Users</Link>
-    </div>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: '#333',
+      color: '#fff',
+      padding: '1rem',
+      '& a': {
+        color: '#fff',
+        textDecoration: 'none',
+        '&:hover': {
+          backgroundColor: '#555',
+        }
+      }
+    }}>
+      <Typography variant="h6">Header</Typography>
+      <Stack direction="row" spacing={2}>
+        <Link to="/login">Login</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/logout">Logout</Link>
+      </Stack>
+    </Box>
   )
 }
