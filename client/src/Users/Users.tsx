b85b8717ca364baf7c0dbf3fc5b83d960fc52531
max-im <pogidaevmo@gmail.com>
2024-05-19 14:39:33 +0300

    add pagination

--- client/src/Users/Users.tsx
+++ client/src/Users/Users.tsx
@@ -1,9 +1,11 @@
-import { Table, TableCell, TableContainer, TableRow } from '@mui/material';
+import { Table, TableCell, TableContainer, TableFooter, TablePagination, TableRow } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Users() {
+  const [page, setPage] = useState(0);
+  const [perPage, setPerPage] = useState(10);
   const [users, setUsers] = useState([]);
   useEffect(() => {
     axios.get('/ambassadors').then(({data}) => setUsers(data))
@@ -13,21 +15,33 @@ export default function Users() {
       <TableContainer sx={{mt:2}}>
         <Table>
           <TableRow>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
+            <TableCell>#</TableCell>
+            <TableCell>Name</TableCell>
             <TableCell>Email</TableCell>
             <TableCell>Action</TableCell>
           </TableRow>
-          {users.map((user: any) => (
+          {users.slice(page*perPage, (page+1)*perPage).map((user: any) => (
             <TableRow key={user.id}>
-              <TableCell>{user.first_name}</TableCell>
-              <TableCell>{user.last_name}</TableCell>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.first_name} {user.last_name}</TableCell>
               <TableCell>{user.email}</TableCell>
               <TableCell>
                 <Link to={`/users/${user.id}`}>View</Link>
               </TableCell>
             </TableRow>
           ))}
+          <TableFooter>
+            <TablePagination
+              component="div"
+              count={users.length}
+              rowsPerPageOptions={[10, 20, 30]}
+              onRowsPerPageChange={(e) => setPerPage(Number(e.target.value))}
+              page={page}
+              rowsPerPage={perPage}
+              onPageChange={(_, page) => setPage(page)}
+            />
+
+          </TableFooter>
         </Table>
       </TableContainer>
   </div>;
